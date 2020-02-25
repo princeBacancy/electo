fef5d3106c72140f4c005b825edb4481827dc5dd
prince <prince.rabadiya@bacancytechnology.com>
2020-02-25 10:41:00 +0530

    election validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@super_admin = User.create( email: "princerabadiya75@gmail.com", 
+                        created_at: "2020-02-24 05:55:03", 
+                        user_name: "prince_the_admin", 
+                        first_name: "prince", 
+                        last_name: "rabadiya", 
+                        gender: "Male", 
+                        password: "8140527507420",
+                        birth_date: "1999-10-08")
+
+@super_admin.add_role :super_admin
