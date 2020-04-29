ca73e092800a568c8c16be164ccddfbde77b8737
prince <prince.rabadiya@bacancytechnology.com>
2020-04-29 11:01:11 +0530

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
-@super_admin = User.create_with(
+@super_admin = User.new(
+  email: 'princerabadiya75@gmail.com',
   user_name: 'prince_the_admin',
   first_name: 'prince',
   last_name: 'rabadiya',
   gender: 'Male',
   password: '8140527507420',
   birth_date: '1999-10-08'
-).find_or_create_by(email: 'princerabadiya75@gmail.com')
-
+)
+@super_admin.save!
 @super_admin.add_role :super_admin
